e10ae16248e29bbff57ca2fb1b74f13261964e15
adriano <adrianobinan@gmail.com>
2026-06-23 09:33:16 -0300

    class model salesman finalizada

---
+++ src/main/java/model/SalesMan.java
@@ -0,0 +1,53 @@
+package model;
+
+public class SalesMan {
+	
+	//Atributs
+	
+	private int salesManId;
+	private String name;
+	private String city;
+	private double commission;
+	
+	//Constructors
+	
+	public SalesMan() {
+		
+	}
+	
+	public SalesMan(int salesManId, String name, String city, double commission) {
+		this.salesManId = salesManId;
+		this.name = name;
+		this.city = city;
+		this.commission = commission;
+	}
+	
+	// Getters and Setters
+
+	public int getSalesMan() {
+		return salesManId;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	public String getCity() {
+		return city;
+	}
+	public double getCommssion() {
+		return commission;
+	}
+	
+	@Override
+	
+	public String toString() {
+        return "SalesMan{" +
+               "salesmanId="   + salesManId  +
+               ", name='"      + name        + '\'' +
+               ", city='"      + city        + '\'' +
+               ", commission=" + commission  +
+               '}';
+    }
+
+
+}
